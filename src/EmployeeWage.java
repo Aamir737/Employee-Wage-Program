d84fa8f47ea77cef22ac41131c5a7696e6f56c8f
unknown <shaikh.ayan737@gmail.com>
2021-08-11 20:49:26 +0530

    Present Absent

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -4,6 +4,14 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
+		int IsPresent=1;
+		double empchek = Math.floor(Math.random()*10)%2;
+		if (empchek == IsPresent) {
+			System.out.println("Employee is Present");
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
 
 	}
 
